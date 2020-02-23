a695fa9507a7a8325432ba4dc60ea22afbc5dacd
Chris Lin <chris_lin@trend.com.tw>
2020-02-23 18:43:42 +0800

    Create create_budget feature and steps

---
+++ AccountingWeb/features/steps/create_budget.py
@@ -0,0 +1,17 @@
+from behave import *
+
+@given('create a budget')
+def step_impl(context):
+    pass
+
+@when('I create')
+def step_impl(context):
+    pass
+
+@then('Then it should be created successfully')
+def step_impl(context):
+    pass
+
+@then('there should be budgets existed')
+def step_impl(context):
+    pass
